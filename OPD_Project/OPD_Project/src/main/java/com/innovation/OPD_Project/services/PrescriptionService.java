326e2fb1504b9b115c3e4190ce0c50bda2f02bde
Gulnar-123 <gulnar6816@gmail.com>
2024-07-10 17:10:23 +0530

    backend

---
+++ OPD_Project/OPD_Project/src/main/java/com/innovation/OPD_Project/services/PrescriptionService.java
@@ -0,0 +1,15 @@
+package com.innovation.OPD_Project.services;
+
+import java.util.List;
+
+import com.innovation.OPD_Project.model.Appointment;
+import com.innovation.OPD_Project.model.Prescription;
+
+public interface PrescriptionService {
+	Prescription insert(Prescription ps);
+	
+	Prescription update(long pid,Prescription ps);
+	Prescription search(long pid);
+	List<Prescription> getAll();
+
+}
